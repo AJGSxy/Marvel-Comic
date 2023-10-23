24dd2e2078a76f3bfcd0acedc5c7be18fdc27c29
ASGJxy <armasjack11@gmail.com>
2023-10-23 16:04:48 -0500

    feat: add input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,14 @@ import styles from "./searchbar.module.css";
 export const SearchBar = () => {
   return (
     <header>
-      <div className={styles.searchBox}>Hola</div>
+      <div className={styles.searchBox}>
+        <div className={styles.logo}>hola</div>
+        <input
+          className={styles.searchInput}
+          type="search"
+          placeholder="Buscar"
+        />
+      </div>
     </header>
   );
 };
