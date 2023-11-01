7b28a170b7ccf3b3a6181386cb761a4d117b64f0
ASGJxy <armasjack11@gmail.com>
2023-11-01 15:05:40 -0500

    feat: new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { Card } from "./components/Card";
 import { SearchBar } from "./components/SearchBar";
+import styles from "./app.module.css";
 const characters = [
   { id: "0", name: "gambito", img: "gambit.jpg" },
   { id: "1", name: "spiderman", img: "spiderman.jpg" },
   { id: "2", name: "wolverine", img: "wolverine.jpg" },
   { id: "3", name: "ironman", img: "ironman.jpg" },
+  { id: "4", name: "gambito", img: "gambit.jpg" },
+  { id: "5", name: "spiderman", img: "spiderman.jpg" },
+  { id: "6", name: "wolverine", img: "wolverine.jpg" },
+  { id: "7", name: "ironman", img: "ironman.jpg" },
+  { id: "8", name: "gambito", img: "gambit.jpg" },
+  { id: "9", name: "spiderman", img: "spiderman.jpg" },
+  { id: "10", name: "wolverine", img: "wolverine.jpg" },
+  { id: "11", name: "ironman", img: "ironman.jpg" },
 ];
 
 function App() {
   return (
     <>
       <SearchBar />
-      {characters.map((character) => {
-        return (
-          <Card name={character.name} img={character.img} key={character.id} />
-        );
-      })}
+      <div className={styles.backgroundContainer}>
+        <div className={styles.cardsContainer}>
+          {characters.map((character) => {
+            return (
+              <Card
+                name={character.name}
+                img={character.img}
+                key={character.id}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
