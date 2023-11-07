2fa557487893c66c10f1d7f5c5a35bdb9a0a81a4
ASGJxy <armasjack11@gmail.com>
2023-11-07 10:08:58 -0600

    add: fav icon

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import styles from "./card.module.css";
 // eslint-disable-next-line
 export const Card = ({ characterName, img }) => {
+  const [isFavorited, setIsFavorited] = useState(false);
   return (
     <div className={styles.cardBox}>
-      <img src={img} width={200} height={300} />
-
-      {/* <div
+      <div
+        className={styles.imageContainer}
         style={{
-          backgroundImage: `linear-gradient(to top, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url(${img})`,
-          width: 200,
-          height: 300,
-          backgroundSize: "cover",
+          backgroundImage: `linear-gradient(41deg, rgba(0, 0, 0, 0.33) 0%, rgba(255, 255, 255, 0.33) 50%, rgba(0, 0, 0, 0.33) 100%), url(${img})`,
           color: "white",
         }}
-      /> */}
+      />
       <div className={styles.characterName}>{characterName}</div>
+      <button
+        className={styles.favoriteButton}
+        onClick={() => setIsFavorited(!isFavorited)}
+      >
+        {isFavorited ? "★" : "☆"}
+      </button>
     </div>
   );
 };
